00a0b3f634ed9aa853108320ab04efe68866ef5b
Adam <adamgensh@gmail.com>
2019-11-21 23:36:47 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+var LiveReloadPlugin = require('webpack-livereload-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: process.env.NODE_ENV || 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: '/',
+    library: 'Demo',
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new LiveReloadPlugin(),
+    new HtmlWebpackPlugin(),
+  ]
+};
